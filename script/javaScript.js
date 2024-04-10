e5023da4d3e347254070a73b0a3e4df0b114a73c
Michał <108900680+Mdrozak@users.noreply.github.com>
2024-04-10 22:08:32 +0200

    Code refactor, change functionality of player hand selection

--- script/javaScript.js
+++ script/javaScript.js
@@ -6,9 +6,32 @@ Create function that plays a single round of Rock paper Scissors
 
 let selectionEvent = document.querySelector("#selection")
 
-let playerHand = selectionEvent.addEventListener('click', function(event) {
-    if (event.target.id !== "selection") return playRound(event.target.id, getComputerChoice())
-    else console.log("click on buttons!")
+let playerScore = 0
+let computerScore = 0
+
+selectionEvent.addEventListener('click', function(event) {
+    let roundAnnouncement = document.querySelector("#round-result")
+    let playerScoreDisplayed = document.querySelector("#player-score")
+    let computerScoreDisplayed = document.querySelector("#computer-score")
+
+    if (event.target.id !== "selection") {
+        let round = playRound(event.target.id, getComputerChoice())
+
+        roundAnnouncement.textContent = round
+
+        if(round.indexOf("Win!") >= 0) {
+            playerScore = playerScore += 1
+            playerScoreDisplayed.textContent = `Players score is: ${playerScore}`
+        }
+        else if (round.indexOf("Draw") < 0) {
+            computerScore = computerScore += 1
+            computerScoreDisplayed.textContent = `Computer score is: ${computerScore}`
+        }
+
+        checkScore(playerScore, computerScore)
+    }
+    else roundAnnouncement.textContent = "Click on buttons!"
+    
 })
 
 function getComputerChoice () {
@@ -21,37 +44,31 @@ function getComputerChoice () {
 function playRound (playerSelection, computerSelection) {
 
     if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "paper") {
-        console.log("You Lose! Paper beats Rock")
         return "You Lose! Paper beats Rock"
     }
     else if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "scissor") {
-        console.log("You Win! Rock beats Scissors")
         return "You Win! Rock beats Scissors"
     }
     else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock") {
-        console.log("You Win! Paper beats Rock")
         return "You Win! Paper beats Rock"
     }
     else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "scissor") {
-        console.log("You Lose! Scissors beats Paper")
         return "You Lose! Scissors beats Paper"
     }
     else if (playerSelection.toLowerCase() === "scissor" && computerSelection.toLowerCase() === "rock") {
-        console.log("You Lose! Rock beats Scissors")
         return "You Lose! Rock beats Scissors"
     }
     else if (playerSelection.toLowerCase() === "scissor" && computerSelection.toLowerCase() === "paper") {
-        console.log("You Win! Scissors beats Paper")
         return "You Win! Scissors beats Paper"
     }
     else {
-        console.log("Draw!")
         return "Draw!"
     }
 }
 
-function playGame () {
-
+function checkScore (playerScore, computerScore) {
+    if (playerScore === 5 || computerScore === 5) {
+        let containerText = document.querySelector("#round-result")
+        playerScore > computerScore ? containerText.textContent = "Player won!" : containerText.textContent = "Computer won!"
+    }
 }
-
-playGame()
